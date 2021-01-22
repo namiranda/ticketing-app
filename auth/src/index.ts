d4b5e8796af1e171e49b2083791fc6cfe2cd21ea
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-22 16:33:48 -0300

    agrego db para auth + User model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -16,6 +17,21 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(errorHandler);
 
+const start = async () => {
+  try {   
+      await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true
+      });
+      console.log('Connected to MongoDb');
+  } catch (err) {
+      console.log(err);
+  }  
+};
+
 app.listen(3000, ()=>{
-    console.log('Listening on port 3000!')
+    console.log('Listening on port 3000!');
 })
+
+start();
