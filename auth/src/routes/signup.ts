979dfab8d250cb717bcd7f3612736c6c639f5d22
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-24 17:12:14 -0300

    agrego BadRequestError y arreglo cosas

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from 'express-validator';
 import { User } from '../models/User'
 import { RequestValidationError} from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
-    '/api/users/signup', [
+    '/api/users/signup', 
+    [
         body('email')
             .isEmail()
             .withMessage('Email must be valid'),
@@ -27,8 +29,7 @@ router.post(
     //En caso contrario existing user va a ser null
 
     if(existingUser) {
-        console.log('Email in use');
-        return res.send({});
+        throw new BadRequestError('Email in use');
     }
 
     const user = User.build({ email, password});
