7279afc9859cb7ce20b5d5d300ffe88dc5ac858a
namiranda <49685356+namiranda@users.noreply.github.com>
2021-01-26 20:54:59 -0300

    agrego current user logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res)=>{
-    res.send('hello there');
+    if(!req.session?.jwt) {
+        return res.send({ currentuser: null });
+    }
+
+    try {
+        const payload = jwt.verify(
+            req.session.jwt, 
+            process.env.JWT_KEY!
+        );
+        res.send({ currentuser: payload });
+    } catch (err){
+        res.send({ currentuser: null });
+    }
 });
 
 export {router as currentUserRouter};
