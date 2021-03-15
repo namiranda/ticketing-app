8db6781060413c6505d96b06dbddcb45ebb1fac7
namiranda <naylenmiranda@gmail.com>
2021-03-15 20:34:30 -0300

    borro common

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { requireAuth } from '@namorg/common';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, requireAuth, (req, res)=>{
+router.get('/api/users/currentuser', currentUser, (req, res)=>{
    res.send({ currentUser: req.currentUser || null})
 });
 
